b8af98b111a9f936761ce2725e500c216dc6be8e
Park WonJong <kidsmon@naver.com>
2026-03-28 21:51:06 +0900

    Init Project

---
+++ Source/SimpleModule/SimpleModule.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SimpleModule : ModuleRules
+{
+	public SimpleModule(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"SimpleModule",
+			"SimpleModule/Variant_Strategy",
+			"SimpleModule/Variant_Strategy/UI",
+			"SimpleModule/Variant_TwinStick",
+			"SimpleModule/Variant_TwinStick/AI",
+			"SimpleModule/Variant_TwinStick/Gameplay",
+			"SimpleModule/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/SimpleModule/SimpleModule.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleModule, "SimpleModule" );
+
+DEFINE_LOG_CATEGORY(LogSimpleModule)
+ 
