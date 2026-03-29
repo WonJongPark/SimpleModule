b5601057ee80852342bbaf1a6d6414508f13b44a
Park WonJong <kidsmon@naver.com>
2026-03-29 15:29:04 +0900

    [01] 모듈 생성

---
+++ Source/MySpartaLog/MySpartaLog.Build.cs
@@ -0,0 +1,25 @@
+﻿using UnrealBuildTool;
+
+public class MySpartaLog : ModuleRules
+{
+    public MySpartaLog(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                // Initial Modules
+                "Core", "CoreUObject", "Engine", "InputCore",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Slate",
+                "SlateCore",
+            }
+        );
+    }
+}
---
+++ Source/MySpartaLog/Private/MySpartaLog.cpp
@@ -0,0 +1,20 @@
+﻿// MySpartaLog.cpp
+
+#include "MySpartaLog.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(LogMySpartaModule)
+
+void FMySpartaLogModule::StartupModule()
+{
+    UE_LOG(LogMySpartaModule, Warning, TEXT("MySpartaLog is Start"));
+}
+
+void FMySpartaLogModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FMySpartaLog, MySpartaLog);
